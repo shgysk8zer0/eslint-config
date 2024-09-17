3e782416cd3ac48f6354a9ef4114e8cae19ef80e
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-17 07:52:21 -0700

    Update globals and use node 20.9.0

--- globals.js
+++ globals.js
@@ -1,4 +1,33 @@
 import globals from 'globals';
 
-export const browser = globals.browser;
-export const node = globals.node;
+export const browser = {
+	AggregateError: false,
+	ElementInternals: false,
+	Iterator: false,
+	Lock: false,
+	LockManager: true,
+	PasswordCredential: false,
+	Scheduler: false,
+	TrustedHTML: false,
+	TrustedScript: false,
+	TrustedScriptURL: false,
+	TrustedTypePolicy: false,
+	TrustedTypePolicyFactory: false,
+	URLPattern: false,
+	cookieStore: false,
+	launchQueue: false,
+	scheduler: false,
+	showOpenFilePicker: false,
+	showSaveFilePicker: false,
+	trustedTypes: false,
+	...globals.browser,
+};
+
+export const node = {
+	AggregateError: false,
+	Iterator: false,
+	Scheduler: false,
+	URLPattern: false,
+	scheduler: false,
+	...globals.node,
+};
