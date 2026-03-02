da87db9b25efb9ca021303ba6ae0fb8c4e1c5e5e
Chris Zuber <shgysk8zer0@gmail.com>
2026-03-02 10:34:47 -0800

    Export correct environmant config

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ export { rules } from './rules.js';
 export * as globals from './globals.js';
 export * as languageOptions from './languageOptions.js';
 export const node = ({ ignores = [], ...config } = {}) => [{ ignores: [...ignoreFile.ignores, ...ignores] }, nodeConfig(config)];
-export const browser = ({ ignores = [], ...config } = {}) => [{ ignores: [...ignoreFile.ignores, ...ignores] }, nodeConfig(config)];
+export const browser = ({ ignores = [], ...config } = {}) => [{ ignores: [...ignoreFile.ignores, ...ignores] }, browserConfig(config)];
 export { ignoreFile, nodeConfig, browserConfig };
